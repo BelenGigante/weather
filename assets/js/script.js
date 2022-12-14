5b33e7765b65ff7b20212c740656d5c4c94586e6
BelenGigante <vinn_a@hotmail.com>
2022-12-14 00:22:12 -0500

    converted temperature to imperial

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,24 @@ var typeCityEl = document.querySelector(".inputValue");
 var dContainer = document.querySelector("#daily-container")
 var formEl = document.querySelector("#form");
 var btnEl = document.querySelector(".btn");
-var name = document.querySelector(".name");
+var cityName = document.querySelector(".name");
 var desc = document.querySelector(".desc");
 var temp = document.querySelector(".temp");
 
 btnEl.addEventListener("click", function (event) {
     event.preventDefault();
-    var api = "http://api.openweathermap.org/data/2.5/weather?q=" + typeCityEl.value + "&appid=51007bcd6af627372cf7b54eb1273ace";
+    var api = "http://api.openweathermap.org/data/2.5/weather?q=" + typeCityEl.value + "&appid=51007bcd6af627372cf7b54eb1273ace&units=imperial";
     fetch(api)
-  
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+        var nameInfo = data["name"];
+        var tempInfo = data["main"]["temp"];
+        var descInfo = data["weather"][0]["description"];
+        cityName.innerHTML = nameInfo;
+        temp.innerHTML = tempInfo;
+        desc.innerHTML = descInfo;
+    })
 
     //.catch(error => alert("wrong city name"))
 })
