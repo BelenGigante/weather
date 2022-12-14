b9b40180f2f1740d066296e8b3fc7a48ad422e58
BelenGigante <vinn_a@hotmail.com>
2022-12-13 23:49:35 -0500

    erased broken code and started again fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,18 @@
-var typeCity= document.querySelector("#typeCity");
+var typeCityEl = document.querySelector(".inputValue");
 var dContainer = document.querySelector("#daily-container")
+var formEl = document.querySelector("#form");
+var btnEl = document.querySelector(".btn");
+var name = document.querySelector(".name");
+var desc = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
 
-var getCityWeather = function(city){
-    var weatherApi = "https://accuweatherstefan-skliarovv1.p.rapidapi.com/get24HoursConditionsByLocationKey";
-    
-    fetch(weatherApi)
-    .then(function(outcome){
-        if (outcome.ok){
-            outcome.json().then(function(data){
-                console.log("city weather",data);
-                displayWeather();
-            });
-        }else{
-            alert("Error " + outcome.statusText);
-        }
-    })
-    .catch(function(error){
-        alert("unable to reach server");
-    });
-};
-
-var formEnter = function(event){
+btnEl.addEventListener("click", function (event) {
     event.preventDefault();
-    var nameCity= typeCity.ariaValueMax.trim();
-    if(nameCity){
-        getCityWeather(nameCity);
-        dContainer.textContent="";
-        nameCity.value="";
-    }  else{
-        alert("enter valid city");
-    }
-};
+    var api = "http://api.openweathermap.org/data/2.5/weather?q=" + typeCityEl.value + "&appid=51007bcd6af627372cf7b54eb1273ace";
+    fetch(api)
+  
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+    //.catch(error => alert("wrong city name"))
+})
