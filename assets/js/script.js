7bb92282815531246d3658b404ada42df29e635f
BelenGigante <vinn_a@hotmail.com>
2022-12-14 01:31:39 -0500

    making for loops to display 5 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ var btnEl = document.querySelector(".btn");
 var cityName = document.querySelector(".name");
 var desc = document.querySelector(".desc");
 var temp = document.querySelector(".temp");
+var minValues = document.querySelector(".minValues");
+var maxValues = document.querySelector(".maxValues");
 
 btnEl.addEventListener("click", function (event) {
     event.preventDefault();
@@ -19,6 +21,26 @@ btnEl.addEventListener("click", function (event) {
         cityName.innerHTML = nameInfo;
         temp.innerHTML = tempInfo;
         desc.innerHTML = descInfo;
+
+    })
+    var apiFive = "http://api.openweathermap.org/data/2.5/forecast?q=" + typeCityEl.value + "&appid=51007bcd6af627372cf7b54eb1273ace&units=imperial";
+    fetch(apiFive)
+    .then(response => response.json())
+    .then(data => {
+        for (i=0; i<5;i++){
+            document.getElementById("dailyMin").innerHTML= "Min : " + Number(data.list[i].main.temp_min);
+        }
+        /*console.log(data);
+        var nameInfo = data["name"];
+        var tempInfo = data["main"]["temp"];
+        var tMax = data["main"]["temp_max"];
+        var tMin = data["main"]["temp_min"];
+        var descInfo = data["weather"][0]["description"];
+        cityName.innerHTML = nameInfo;
+        temp.innerHTML = tempInfo;
+        desc.innerHTML = descInfo;
+        minValues.innerHTML = tMin;
+        maxValues.innerHTML = tMax;*/
     })
 
     //.catch(error => alert("wrong city name"))
